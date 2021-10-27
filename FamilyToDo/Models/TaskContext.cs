ca87603c0a8506e612258ed1cf92a5fe20913ea8
Steven Mather <stevenrmather@gmail.com>
2021-10-27 09:14:00 -0700

    Initial project creation

---
+++ FamilyToDo/Models/TaskContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FamilyToDo.Models
+{
+    public class TaskContext :DbContext
+    {
+        public TaskContext(DbContextOptions<TaskContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Task>Tasks { get; set; }
+        public DbSet<User>Users { get; set; }
+    }
+}
